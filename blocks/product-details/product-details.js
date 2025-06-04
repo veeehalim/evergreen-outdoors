a707a1065cb658ced80aa290e25fb345beb51616
Alisa Roussau <aroussau@deloitte.com.au>
2025-06-04 21:08:20 +1000

    Testing JS updates.

--- blocks/product-details/product-details.js
+++ blocks/product-details/product-details.js
@@ -1,40 +1,38 @@
-export default function decorate(block) {
-  // Set up image container
-  const imageContainer = document.querySelector('.product-details > div:first-child');
-  imageContainer.classList.add('product-image');
+// Set up image container
+const imageContainer = document.querySelector('.product-details > div:first-child');
+imageContainer.classList.add('product-image');
 
-  // Set up product information container
-  const productInfoWrapper = document.createElement('div');
-  productInfoWrapper.classList.add('product-attributes');
-  const productInfo = document.querySelectorAll('.product-details > div:not(:first-child)')
-  productInfo.forEach((element) => {
-    productInfoWrapper.append(element);
-  });
+// Set up product information container
+const productInfoWrapper = document.createElement('div');
+productInfoWrapper.classList.add('product-attributes');
+const productInfo = document.querySelectorAll('.product-details > div:not(:first-child)')
+productInfo.forEach((element) => {
+  productInfoWrapper.append(element);
+});
 
-  // Add buttons
-  const addToCartBtn = document.createElement('a');
-  addToCartBtn.innerHTML = `<a href="#" title="+ Add to bag" class="button cart-button">+ Add to bag</a>`;
-  const storeAvailabilityBtn = document.createElement('a');
-  storeAvailabilityBtn.innerHTML = `<a href="#" title="Check store availability" class="button secondary">Check store availability</a>`;
-  productInfoWrapper.append(addToCartBtn);
-  productInfoWrapper.append(storeAvailabilityBtn);
+// Add buttons
+const addToCartBtn = document.createElement('a');
+addToCartBtn.innerHTML = `<a href="#" title="+ Add to bag" class="button cart-button">+ Add to bag</a>`;
+const storeAvailabilityBtn = document.createElement('a');
+storeAvailabilityBtn.innerHTML = `<a href="#" title="Check store availability" class="button secondary">Check store availability</a>`;
+productInfoWrapper.append(addToCartBtn);
+productInfoWrapper.append(storeAvailabilityBtn);
 
-  // Add product information container next to image container
-  block.append(productInfoWrapper);
+// Add product information container next to image container
+block.append(productInfoWrapper);
 
-  // Remove unnecessary divs
-  [...productInfoWrapper.children].forEach((node) => {
-    node.replaceWith(...node.childNodes);
-  });
+// Remove unnecessary divs
+[...productInfoWrapper.children].forEach((node) => {
+  node.replaceWith(...node.childNodes);
+});
 
-  // Add classes and update innerHTML for key product information items
-  const productTitle = document.querySelector('.product-attributes > div:first-child');
-  productTitle.innerHTML = '<h1>' + productTitle.children[0].innerHTML + '</h1>';
-  productTitle.classList.add('product-title');
+// Add classes and update innerHTML for key product information items
+const productTitle = document.querySelector('.product-attributes > div:first-child');
+productTitle.innerHTML = '<h1>' + productTitle.children[0].innerHTML + '</h1>';
+productTitle.classList.add('product-title');
 
-  const productPrice = document.querySelector('.product-attributes > div:nth-child(3)');
-  productPrice.innerHTML = '<p>$' + productPrice.textContent + '</p>';
-  productPrice.classList.add('product-price');
+const productPrice = document.querySelector('.product-attributes > div:nth-child(3)');
+productPrice.innerHTML = '<p>$' + productPrice.textContent + '</p>';
+productPrice.classList.add('product-price');
 
-  console.log("Product details JS has loaded");
-}
+console.log("Product details JS has loaded");
