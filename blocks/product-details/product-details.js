2cf238ab590743a70645365dfbef46d4c75ba299
Alisa Roussau <aroussau@deloitte.com.au>
2025-06-04 21:47:08 +1000

    Added console.logs.

--- blocks/product-details/product-details.js
+++ blocks/product-details/product-details.js
@@ -1,6 +1,6 @@
 export default function decorate(block) {
   const productDetails = document.querySelector('.product-details > .product-attributes');
-  console.log(productDetails);
+  console.log("Product details: " + productDetails);
 
   // Set up image container
   const imageContainer = document.querySelector('.product-details > div:first-child');
@@ -9,10 +9,12 @@ export default function decorate(block) {
   // Set up product information container
   const productInfoWrapper = document.createElement('div');
   productInfoWrapper.classList.add('product-attributes');
-  const productInfo = document.querySelectorAll('.product-details > div:not(:first-child)')
+  const productInfo = document.querySelectorAll('.product-details > div:not(:first-child)');
+  console.log("Product info wrapper before elements appended: " + productInfo);
   productInfo.forEach((element) => {
     productInfoWrapper.append(element);
   });
+  console.log("Product info wrapper after elements appended: " + productInfoWrapper);
 
   // Add buttons
   const addToCartBtn = document.createElement('a');
@@ -21,27 +23,35 @@ export default function decorate(block) {
   storeAvailabilityBtn.innerHTML = `<a href="#" title="Check store availability" class="button secondary">Check store availability</a>`;
   productInfoWrapper.append(addToCartBtn);
   productInfoWrapper.append(storeAvailabilityBtn);
+  console.log("Product info wrapper after buttons appended: " + productInfoWrapper);
 
   // Remove unnecessary divs
   [...productInfoWrapper.children].forEach((node) => {
     node.replaceWith(...node.childNodes);
   });
+  console.log("Product info wrapper after nodes replaced: " + productInfoWrapper);
 
   // Add product information container next to image container
   if (productDetails == null) {
     block.append(productInfoWrapper);
+    console.log("product details is null");
   } else {
     productDetails.replaceWith(productInfoWrapper);
+    console.log("product details is not null");
   }
 
   // Add classes and update innerHTML for key product information items
   const productTitle = document.querySelector('.product-attributes > div:first-child');
+  console.log("Product title before transformation: " + productTitle);
   productTitle.innerHTML = '<h1>' + productTitle.children[0].innerHTML + '</h1>';
   productTitle.classList.add('product-title');
+  console.log("Product title after transformation: " + productTitle);
 
   const productPrice = document.querySelector('.product-attributes > div:nth-child(3)');
+  console.log("Product price before transformation: " + productPrice);
   productPrice.innerHTML = '<p>$' + productPrice.textContent + '</p>';
   productPrice.classList.add('product-price');
+  console.log("Product price after transformation: " + productPrice);
 
   console.log("Product details JS has loaded");
 }
