fe81fdd5ea42df297defe5efa13c64229138c927
Alisa Roussau <aroussau@deloitte.com.au>
2025-06-05 16:11:03 +1000

    Added product-details component.

---
+++ blocks/product-details/product-details.js
@@ -0,0 +1,48 @@
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
+export default function decorate(block) {
+  const productDetails = document.querySelector('.product-details > .product-attributes');
+
+  // Set up image container
+  const imageContainer = document.querySelector('.product-details > div:first-child');
+  imageContainer.classList.add('product-image');
+
+  // Set up product information container
+  const productInfoWrapper = document.createElement('div');
+  productInfoWrapper.classList.add('product-attributes');
+  const productInfo = document.querySelectorAll('.product-details > div:not(.product-image, .product-attributes, :first-child)');
+  productInfo.forEach((element) => {
+    productInfoWrapper.append(element);
+  });
+
+  // Add buttons
+  const addToCartBtn = document.createElement('a');
+  addToCartBtn.innerHTML = `<a href="#" title="+ Add to bag" class="button cart-button">+ Add to bag</a>`;
+  const storeAvailabilityBtn = document.createElement('a');
+  storeAvailabilityBtn.innerHTML = `<a href="#" title="Check store availability" class="button secondary">Check store availability</a>`;
+  productInfoWrapper.append(addToCartBtn);
+  productInfoWrapper.append(storeAvailabilityBtn);
+
+  // Remove unnecessary divs
+  [...productInfoWrapper.children].forEach((node) => {
+    node.replaceWith(...node.childNodes);
+  });
+
+  // Add containers generated to block
+  if (productDetails == null) {
+    block.append(productInfoWrapper);
+  } else {
+    block.querySelector('div:first-child').replaceWith(imageContainer);
+    moveInstrumentation(productDetails, productInfoWrapper);
+    block.append(productInfoWrapper);
+  }
+
+  // Add classes and update innerHTML for key product information items
+  const productTitle = document.querySelector('.product-attributes > div:first-child:not(.product-title)');
+  productTitle.innerHTML = '<h1>' + productTitle.children[0].innerHTML + '</h1>';
+  productTitle.classList.add('product-title');
+
+  const productPrice = document.querySelector('.product-attributes > div:nth-child(3)');
+  productPrice.innerHTML = '<p>$' + productPrice.textContent + '</p>';
+  productPrice.classList.add('product-price');
+}
