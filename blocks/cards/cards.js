753ff0c80ef0cb8403566d0abdbcc1f6b0c3108d
victoriahalim <vhalim@deloitte.com.au>
2025-06-11 10:41:25 +1000

    Add card URL logic

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -6,27 +6,44 @@ export default function decorate(block) {
     "cards-card-title",
     "cards-card-tag",
     "cards-card-price",
+    "cards-card-link",
   ];
   /* change to ul, li */
   const ul = document.createElement("ul");
   [...block.children].forEach((row) => {
-    const li = document.createElement("li");
-    moveInstrumentation(row, li);
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div, index) => {
+    const a = document.createElement("a");
+    moveInstrumentation(row, a);
+    while (row.firstElementChild) a.append(row.firstElementChild);
+    [...a.children].forEach((div, index) => {
       if (div.children.length === 1 && div.querySelector("picture"))
         div.className = "cards-card-image";
       else if (div.children.length > 0) {
         div.className = `cards-card-body ${bodyClassNames[index - 1]}`;
       }
     });
+
+    const li = document.createElement("li");
+    li.appendChild(a);
     ul.append(li);
   });
 
-  // Price styling
-  document.querySelectorAll(".cards-card-price p").forEach((p) => {
+  ul.querySelectorAll(".cards-card-price p").forEach((p) => {
     if (!p.textContent.startsWith("$")) {
       p.textContent = `$${p.textContent}.00`;
+    }
+  });
+
+  const cards = ul.querySelectorAll("li");
+  cards.forEach((card) => {
+    const cardLinkContainer = card.querySelector(".cards-card-link");
+
+    if (cardLinkContainer) {
+      const cardLink = cardLinkContainer
+        .querySelector("a")
+        .getAttribute("title");
+      card.querySelector("a").setAttribute("href", cardLink);
+      card.querySelector("a").setAttribute("target", "_blank");
+      cardLinkContainer.remove();
     }
   });
 
